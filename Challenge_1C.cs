310c92ef6aa82e9aa097b8c43dc7204d52e2c881
Natalie Schulze <Nschulze1997@gmail.com>
2022-09-20 14:55:58 -0600

    Create Challenge_1C.cs
    Playing around with arrays and loops.

---
+++ Challenge_1C.cs
@@ -0,0 +1,35 @@
+using System;
+					
+public class Program
+{
+	public void main()
+	{
+    int i = 0;
+    Console.WriteLine("Welcome players! You are now trapped in a room. Water is growing inside.");
+    characters();
+    
+     if(i => 10)
+    {
+      Console.WriteLine("The room is flooded. You are dead...sorry.");
+    }
+  else do 
+  {
+    Console.WriteLine("The water is rising! Current water level is" + i);
+     i++;
+   }
+    while (i < 10);
+    }
+    
+   public void characters()
+   {
+   string[] names = {"Stacy", "Steve", "Robin", "Dustin"};
+    for (int i = 0; i < names.Length; i++) 
+      {
+        Console.WriteLine(names[i]);
+      }
+      foreach (string i in cars) 
+        {
+          Console.WriteLine(i);
+        }
+   }
+}
