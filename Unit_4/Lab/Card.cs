d803fe0d31580c303bbdc79a65c4c1425b3c96a2
Natalie Schulze <Nschulze1997@gmail.com>
2022-10-11 16:34:37 -0600

    Create Card.cs
    all the details for creating new cards

---
+++ Unit_4/Lab/Card.cs
@@ -0,0 +1,18 @@
+uisng Systems.Collections;
+using Systems.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+    
+    public Sprite artwork;
+    
+    public int manaCost;
+    public int attack;
+    public int health;
+    
+    
+}
