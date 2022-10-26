6ceec074e1bad8924d85e2ba31eddcb85a829cca
Natalie Schulze <Nschulze1997@gmail.com>
2022-10-25 19:08:00 -0600

    Create Challenge_6.cs
    An object that is created on screen and then descends downwards.

---
+++ UFO_Defenders/Challenge_6.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge_6 : MonoBehavior
+{
+    public gameObject grabbyBoi;
+    
+    void Update()
+    {
+        
+    }
+}
