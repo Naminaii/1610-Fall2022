c6323f517e72628744fda53d591edcc039741521
Natalie Schulze <Nschulze1997@gmail.com>
2022-10-26 22:11:41 -0600

    Update Challenge_6.cs
    Code for a collectable object that is called a starbie, for some reason.  What's a starbie?  Who's to say?

--- UFO_Defenders/Challenge_6.cs
+++ UFO_Defenders/Challenge_6.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Challenge_6 : MonoBehavior
 {
-    public gameObject grabbyBoi;
-    
+    public GameObject grabbyBoi;
+    public float grabbySpeed;
+    public float yRange;
+    public float starbies;
     void Update()
     {
+        transform.Translate(Vector3.down*Time.deltaTime*grabbySpeed);
         
+        if(transform.position.y < -yRange)
+        {
+            Destroy(grabbyBoi);
+        }
+ 
+    }
+    
+    private void OnTriggerEnter(collider grabbyBoi)
+    {
+       starbies =+ 1;
+       
+       Destroy(grabbyBoi.gameObject);
+        
+        Debug.Log("Starbie has been added to inventory");
+        Debug.Log("Starbies are now at:", starbies);
     }
 }
