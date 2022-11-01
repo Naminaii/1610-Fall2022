8391f59a96e4c67604e6e3af8ad52a7fcc4ab722
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-01 13:17:31 -0600

    Update enemyManager.cs

--- Unit6B/Lab6B/enemyManager.cs
+++ Unit6B/Lab6B/enemyManager.cs
@@ -8,13 +8,26 @@ public class enemyManager : MonoBehavior
     private float spawnRangeX = 20f;
     private float posZ = 20f;
     
+    private float startDelay = 2.5f;
+    private float spawnInterval = 2f;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+    }
+    
     void Update()
     {
         if(input.GetKeyDown(KeyCode.S))
         {
-            Vector3 spawnPos = new Vector3(Randomize.Range(-spawnRangeX, spawnRangeX, 0, posZ);
-            int infoIndex = Randomize.Range(0, ufoPrefabs.length); 
-            Substantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
+            SpawnRandomUFO();
         }
+    }
+    
+    void SpawnRandomUFO()
+    {
+        Vector3 spawnPos = new Vector3(Randomize.Range(-spawnRangeX, spawnRangeX, 0, posZ);
+        int infoIndex = Randomize.Range(0, ufoPrefabs.length); 
+        Substantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
     }
 }
