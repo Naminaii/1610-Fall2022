1199f20f35f09975f21848e182c43eb8c8f8f223
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-16 15:54:41 -0700

    Update ID.cs

--- ColorMatch/ID.cs
+++ ColorMatch/ID.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class ID : ScriptableObject
 {
+    public ID idObj;
+    public UnityEvent startEvent;
     
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+    
+    public void startRendererColor(ColorID obj)
+    {
+        rendererObj.color = obj.value;
+    }
 }
