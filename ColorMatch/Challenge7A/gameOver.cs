024bf006b34401c80dd23a80809e202046f31988
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-17 13:03:58 -0700

    Create gameOver.cs
    A script for when a wrong match is made, and all the world ends.

---
+++ ColorMatch/Challenge7A/gameOver.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEvents;
+
+public class gameOver : MonoBehaviour
+{
+    public ID idObj;
+    public UnityEvent GameOver;
+    public Text label;
+    private SpriteRenderer rendererObj;
+    public Color originalColor;
+    
+    private IEnumerator OnTriggerEnter(collider other)
+    {
+        var tempObj = other.GetComponent<IDContainer>();
+        if(tempObj != null) 
+            yield break;
+            
+        var otherID = tempObj.idObj;
+        if(otherID == idObj)
+        {
+            
+        }
+        else
+        {
+            GameOver.Invoke();
+            updateLabel(0 obj);
+            rendererObj.Color = originalColor;
+            yield return new WaitForSeconds(0.5f);
+            EndGame();
+        }
+    }
+
+    public void updateLabel(float obj)
+    {
+        label.text = ToString(CultureInfo.InvariantCulture);
+    }
+    
+    public void updateLabel(int obj)
+    {
+      label.text = ToString(CultureInfo.InvariantCulture);
+    }
+}
