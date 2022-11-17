802f6d83fe1bbc8155205ae25ea3163cc3f89150
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-16 20:43:15 -0700

    Create TransformBehavior.cs
    It's a surprise tool that will help us later.

---
+++ ColorMatch/TransformBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using Systems.Collections.Generic;
+using UnityEngine;
+
+public class TransformBehavior : MonoBehaviour
+{
+    public void resetToZero()
+    {
+        Transform.position = Vector3.zero
+    }
+}
