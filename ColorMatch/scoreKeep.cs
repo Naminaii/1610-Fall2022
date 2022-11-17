9813734a5bca2a998e272b644144e348a0de0781
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-16 20:18:37 -0700

    Update scoreKeep.cs

--- ColorMatch/scoreKeep.cs
+++ ColorMatch/scoreKeep.cs
@@ -1,12 +1,30 @@
+using System;
 using UnityEngine;
+using UnityEvents;
 
 [CreateAssetMenu]
 
 public class scoreKeep : ScriptableObject
 {
     public int value;
+    public UnityEvent disableEvent;
     
+    pubilic void Start(int num)
+    {
+        value = num;
+    }
+    
+    public void setValue(IntData obj)
+    {
+        value = obj.value;
+    }
     public void UpdateValue(int num)
     {
         value += num;
     }
+    
+    private void onDisable()
+    {
+        Debug.Log("End");
+    }
+}
