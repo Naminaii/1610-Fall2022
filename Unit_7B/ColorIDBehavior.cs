e62e689fb0e8fcd75b9bc3f0c5668a2171ddeeab
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-22 16:08:21 -0700

    Create ColorIDBehavior.cs

---
+++ Unit_7B/ColorIDBehavior.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ColorIDBehavior : ID
+{
+    public ColorIDDataList;
+    
+    private void Awake()
+    {
+        idObj = colorIDDataListObj.currentColor;
+    }
+}
