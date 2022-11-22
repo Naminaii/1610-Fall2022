1d363f49a9739e906be7777c398ab96ca4816f40
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-22 13:19:33 -0700

    Update CoroutinesBehavior.cs

--- Unit_7B/CoroutinesBehavior.cs
+++ Unit_7B/CoroutinesBehavior.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CoroutinesBehavior : MonoBehaviour
 {
     public bool canRun = true;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
     
     IEnumerator Start()
     {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFIxedUpdate();
         Debug.Log("Start");
-        yeild return new WaitForSeconds(3);
+        yeild return new wfsObj;
         Debug.Log("Late Start");
         
         while(canRun)
         {
-            yield return new WaitForFixedUpdate();
+            yield return wfsObj;
             Debug.Log("Run on start");
         }
     }
