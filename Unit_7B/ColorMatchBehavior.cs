b15423023c897fd1c8bcb5ff06273dbf8683bc76
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-22 16:14:07 -0700

    Update ColorMatchBehavior.cs

--- Unit_7B/ColorMatchBehavior.cs
+++ Unit_7B/ColorMatchBehavior.cs
@@ -4,8 +4,15 @@ public class ColorMatchBehavior : MatchBehavior
 {
     public ColorIDDataList colorIDDataListObj;
     
+    
     private void Awake()
     {
         idObj = colorIDDataListObj.currentColor;
+    }
+    
+    public void changeColor(SpriteRenderer renderer)
+    {
+       public var newColor = idObj as ColorID;
+       renderer.Color = newColor.value;
     }
 }
