1e977378e372ca192ead90bf3c38e2bb55836633
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-22 16:26:26 -0700

    Create MatchBehavior.cs

---
+++ Unit_7B/Challenge7B/MatchBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchBehavior : MonoBehavior
+{
+    public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent, noMatchDelay;
+    
+    private IEnumerator OnTriggerEnter(collider other)
+    {
+       var tempObj = other.GetComponent<IDContainer>();
+        if(tempObj != null) 
+            yield break;
+       
+       var otherID = tempObj.idObj;  
+       if (otherID == idObj)
+       {
+           matchEvent.Invoke();
+        }
+        else
+            {
+                noMatchEvent.Invoke();
+                yield return new WaitForSeconds(0.5f);
+                noMatchDelay.Invoke();
+            }
+    }
+}
