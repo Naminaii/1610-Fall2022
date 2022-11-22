9307589635024d304623ac305b8f317bdf6073fd
Natalie Schulze <Nschulze1997@gmail.com>
2022-11-22 15:55:10 -0700

    Update colorIDList.cs

--- Unit_7B/colorIDList.cs
+++ Unit_7B/colorIDList.cs
@@ -5,4 +5,29 @@ using UnityEngine;
 public class colorIDList : ScriptableList
 {
     public List<ColorID> colorIDList;
+    
+    public ColorID currentColor;
+    private int num;
+    
+    private void Awake()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+    
+    public void ChangeRendererColor(colorID obj)
+    {
+        rendererObj.color = obj.value;
+        
+    }
+    
+    public void ChangeRendereColor(ColorIDList obj)
+    {
+        rendererObj = obj.currentColor.value;
+    }
+
+    public void SetCurrentColorRando()
+    {
+        num = ColorID.Count -1;
+        currentColor = ColorID[num];
+    }
 }
